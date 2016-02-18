3aca76dd53633903deacea330469302b6272d536
Petri Jokimies <petri.jokimies@iki.fi>
2016-02-18 19:21:36 +0200

    test(karma conf): Run coverage
    Run coverage on file change and with gulp's 'test' task.

--- gulp/tasks/development/karma.js
+++ gulp/tasks/development/karma.js
@@ -5,11 +5,19 @@ gulp.task('test', function(done) {
     new Server({
         configFile: __dirname + '/../../../karma.conf.js',
         singleRun: true,
-        reporters: ['dots', 'junit'],
+        reporters: ['dots', 'junit', 'coverage'],
         junitReporter: {
             outputDir: '',
             outputFile: 'test-results.xml'
         },
+        preprosessors: {
+            '**/portfolio/static/js/**/*.js': ['coverage'],
+        },
+        coverageReporter: {
+            type : 'cobertura',
+            dir : 'coverage/'
+    },
+
         useBrowserName: false,
         browsers: ['PhantomJS']
     }, done).start();
