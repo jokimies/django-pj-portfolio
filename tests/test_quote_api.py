771bf7b9e7c10f40e2104d46ae7af67aa202876c
Petri Jokimies <petri.jokimies@iki.fi>
2018-10-21 14:04:23 +0300

    Mock out get_alpha_vantage_stock_quote
    No need to really go to AV, justmock the return value.
    
    Removed test trying to catch if remote server has stopped working. If it
    does, it will be noticed anyway, test does not really help there.

--- tests/test_quote_api.py
+++ tests/test_quote_api.py
@@ -1,29 +1,35 @@
+try:
+    from unittest.mock import patch
+except ImportError:
+    from mock import patch
+
 from django.core.urlresolvers import reverse
 
 from rest_framework import status
 #
 from .currency_factories import create_currencies
 
+MSFT_RESPONSE = {
+    'date':'2018-10-19',
+    'currency':'USD',
+    'change':'0.16',
+    'change_percentage':'0.147',
+    'ticker':'MSFT',
+    'price':'108.66'
+}
+
 
 class TestQuoteApi:
 
-    def test_api_can_be_called(self, http_client):
-        
-        create_currencies()
+    @patch('portfolio.views.Command.get_alpha_vantage_stock_quote',
+           return_value=MSFT_RESPONSE)
+    def test_api_can_be_called(self, mock_av_quote, http_client):
+        '''Test calling the API succeeds'''
+
+        (currency_eur, currency_usd, currency_history) = create_currencies()
+        # Replace currency with Currency object
+        MSFT_RESPONSE['currency'] = currency_usd
+
         response = http_client.get(reverse('security-quote',
                                            kwargs={'stock': 'MSFT'}))
         assert response.status_code == status.HTTP_200_OK
-
-    def test_api_return_some_content(self, http_client):
-        '''Test the finance API is still working
-
-        The actual code fecthing the quote data naturally uses external
-        service, that might ahve stopped working for good. This test tries
-        to catch those cases.
-        '''
-
-        create_currencies()
-
-        response = http_client.get(reverse('security-quote',
-                                           kwargs={'stock': 'MSFT'}))
-        assert response.data is not {}
